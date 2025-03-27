351119e80cf5e1bcaeb773510969a5dbcc1b7dff
WaseurGIT <waseurrahman31@gmail.com>
2025-03-27 19:39:25 +0600

    dynamic react router

--- introduction-to-react-router/src/components/User/User.jsx
+++ introduction-to-react-router/src/components/User/User.jsx
@@ -1,18 +1,19 @@
-import './User.css'
-const User = ({user}) => {
+import { Link } from "react-router-dom";
+import "./User.css";
+const User = ({ user }) => {
+  const { id, name, email, phone } = user;
 
-    const {id, name, email, phone} = user
-
-    return (
-        <div >
-            <div className="user">
-            <h3>Id: {id}</h3>
-            <h3>Name: {name}</h3>
-            <h4>Email: {email}</h4>
-            <h4>Contact: {phone}</h4>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="user">
+        <h3>Id: {id}</h3>
+        <h3>Name: {name}</h3>
+        <h4>Email: {email}</h4>
+        <h4>Contact: {phone}</h4>
+        <Link to={`/users/${id}`}>Show Details</Link>
+      </div>
+    </div>
+  );
 };
 
 export default User;
